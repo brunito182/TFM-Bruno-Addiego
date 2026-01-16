e5b41839c6364f670ba854fedaad025cc137036e
brunito182 <brunito3427@gmail.com>
2026-01-16 16:30:09 +0100

    Preprocessat.py

--- Preprocessat.py
+++ Preprocessat.py
@@ -2,17 +2,18 @@ import numpy as np
 import librosa
 
 def preprocessat(audio_array, sr, target_sr=44100):
-    # 1. Normalizar a float32 (-1..1)
+    # 1. Normalitzar a float32 (-1..1)
     audio = audio_array.astype(np.float32)
     audio /= np.max(np.abs(audio) + 1e-6)
 
-    # 2. Resamplear
+    # 2. Re-mostreig
     if sr != target_sr:
         audio = librosa.resample(audio, orig_sr=sr, target_sr=target_sr)
         sr = target_sr
 
-    # 3. Trim silencios
+    # 3. Eliminar silencis
     audio, _ = librosa.effects.trim(audio, top_db=30)
 
     return audio, sr
+
 
